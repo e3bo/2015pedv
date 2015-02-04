fb06900d26650088ed406b63834be0438c642a55
Eamon O'Dea <odea35@gmail.com>
2015-02-03 19:08:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+2015pedv
+========
+
+To run the R analysis script within a Docker container with the
+necessary software environment, simply execute the run-scripts script
+from the root directory of this git repository on a system with
+[docker](https://docker.com) installed. The analysis output will be
+placed in a time-stamped directory that begins with "work".
+
+
---
+++ src/clean-shipment-matrix.r
@@ -0,0 +1,21 @@
+stateShipments <- read.csv('../data/StateShipments.csv', skip=2, na.strings="")
+desel <- c("Origin", "Total", "X")
+test <- !(colnames(stateShipments) %in% desel)
+ss <- stateShipments[, test]
+
+desel <- c("Total", "NA.")
+rn <- make.names(stateShipments$Origin)
+test <- !(rn %in% desel)
+ss <- ss[test, ]
+rownames(ss) <- rn[test]
+stopifnot(rownames(ss) == colnames(ss))
+ss[is.na(ss)] <- 0
+
+key <- match(colnames(ss), c(make.names(state.name), 'District.of.Columbia',
+                             'Mexico', 'Canada', 'Other.states'))
+nms <- c(state.abb, 'DC', 'Mexico', 'Canada', 'otherStates')[key]
+
+rownames(ss) <- colnames(ss) <- nms
+write.csv(data.matrix(ss),
+          'shipment-flows-origins-on-rows-dests-on-columns.csv',
+          row.names=TRUE)
