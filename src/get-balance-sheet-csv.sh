cde3ff6c89621025c376b1a4daf7ee802bad267c
Eamon O'Dea <odea35@gmail.com>
2015-02-03 19:59:00 -0500

    Add creation of balance sheet csv

---
+++ src/get-balance-sheet-csv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+file1=$(mktemp)
+file2=$(mktemp)
+outfile='state-hogBalanceSheetDec2000Dec2001.csv'
+archive='/root/data/usda.mannlib.cornell.edu_usda_nass_MeatAnimPr__2000s_2002_MeatAnimPr_04_26_2002.zip'
+
+echo 'state,inventory2000,pigCrop,inshipments' > "${file1}" 
+
+unzip -c "${archive}" meat_014.csv \
+  | grep ,\"d\" \
+  | cut -d',' -f3- \
+  | grep -v ^\"\" \
+  | grep -v ^\"Idaho \
+  | grep -v ^\"Washington \
+  | sed 's/ Washington/IdahoWashington/' >> "${file1}"
+
+echo 'state2,marketings,slaughter,deaths,inventory2001' > "${file2}"
+unzip -c "${archive}" meat_015.csv \
+  | grep ,\"d\" \
+  | cut -d',' -f3- \
+  | grep -v ^\"\" \
+  | grep -v ^\"Idaho \
+  | grep -v ^\"Washington \
+  | sed 's/ Washington/IdahoWashington/' >> "${file2}"
+
+paste --delimiter="," "${file1}" "${file2}" >"${outfile}"
+sed -i -e 's/\r//g' "${outfile}"
+rm "${file1}" "${file2}"
