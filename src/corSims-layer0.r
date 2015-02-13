f6e4ea69141c3e3af2701cb5a2454df1cfe54d19
Eamon O'Dea <odea35@gmail.com>
2015-02-13 17:49:24 -0500

    a

--- src/corSims-layer0.r
+++ src/corSims-layer0.r
@@ -10,9 +10,7 @@ addProblem(reg, id='prob', static=static, dynamic=generateGraph)
 addAlgorithm(reg, id='sim', fun=simAndSummarize)
 
 pdes <- do.call(rbind,
-                list(data.frame(comSize=16, nedges=2^(0:8)),
-                     data.frame(comSize=32, nedges=2^(0:10))))
-#                     data.frame(comSize=64, nedges=2^(0:12))))
+                list(data.frame(comSize=64, nedges=2^(0:12))))
 prob.design <- makeDesign('prob', design=pdes,
  exhaustive=list(
      couplingFactor=c(0.5,1,2),
@@ -27,9 +25,8 @@ addExperiments(reg, prob.designs=prob.design, algo.designs=alg.design, repls=rep
 
 summarizeExperiments(reg)
 
-#chunked <- chunk(getJobIds(reg), chunk.size=20, shuffle=TRUE)
-
-submitJobs(reg, 53000:53002)
+chunked <- chunk(getJobIds(reg), chunk.size=20, shuffle=TRUE)
+submitJobs(reg, chunked)
 
 tmpf <- function(job, res){
     list(cc1=res$crossCor, cohesion=res$cohesion, abConnectivity=res$abConnectivity)
