5e214f8c1bb48b6decbb15b812e48b4c5719d288
Eamon O'Dea <odea35@gmail.com>
2015-10-17 13:52:02 -0400

    Start file of common regression code

--- src/flows.r
+++ src/flows.r
@@ -14,94 +14,17 @@ library(glmmADMB)
 library(ggplot2)
 print(sessionInfo())
 
-#' ## Load data
+#' ## Data prep
+source('regression-modeling-functions.R')
 
-load('common-data.RData')
-
-#' ## Prep data
-
-ObservedMatchingInternalFlows <- function(flows, case.data) {
-    unwanted <- c('week', 'totalNumberSwineAccessions', 'Unk')
-    ind <- which(!colnames(case.data) %in% unwanted)
-    observed <- case.data[, ind]
-    avail <- rownames(flows)[!is.na(flows[, 'impInternalFlow'])]
-    zeros <- setdiff(avail, colnames(observed))
-    observed[, zeros] <- 0
-    observed
-}
-observed <- ObservedMatchingInternalFlows(flows=internal.flows,
-                                          case.data=real.case.data)
-
-ReshapeAugmentObserved <- function(observed, fiAll, flows) {
-    om <- reshape(observed, v.names='cases', varying=colnames(observed),
-                  direction='long', timevar='state', times=colnames(observed),
-                  idvar='week')
-    splt <- split(om, om$state)
-    tmpf <- function(x) {
-        ord <- order(x$week)
-        x <- x[ord, ]
-        x$cumCases <- cumsum(x$cases)
-        x
-    }
-    proc <- lapply(splt, tmpf)
-    om <- do.call(rbind, args=proc)
-    om$nFarms <- fiAll[om$state, ]$X2002.Farms
-    om$nSusc <- om$nFarms - om$cumCases
-    omprev <- om
-    omprev$week <- omprev$week + 1
-    omprev$nFarms <- NULL
-    omprev$cumCases <- NULL
-    c1 <- which(colnames(omprev) == 'cases')
-    c2 <- which(colnames(omprev) == 'nSusc')
-    colnames(omprev)[c1] <- 'cases1WksAgo'
-    colnames(omprev)[c2] <- 'nSusc1WksAgo'
-    om <- merge(om, omprev, by=c('week', 'state'), all.x=TRUE)
-    omprev$week <- omprev$week + 1
-    colnames(omprev)[c1] <- 'cases2WksAgo'
-    colnames(omprev)[c2] <- 'nSusc2WksAgo'
-    om <- merge(om, omprev, by=c('week', 'state'), all.x=TRUE)
-    omprev$week <- omprev$week + 1
-    colnames(omprev)[c1] <- 'cases3WksAgo'
-    colnames(omprev)[c2] <- 'nSusc3WksAgo'
-    om <- merge(om, omprev, by=c('week', 'state'), all.x=TRUE)
-    omprev$week <- omprev$week + 1
-    colnames(omprev)[c1] <- 'cases4WksAgo'
-    colnames(omprev)[c2] <- 'nSusc4WksAgo'
-    om <- merge(om, omprev, by=c('week', 'state'), all.x=TRUE)
-    om$internalFlow <- flows[om$state, "impInternalFlow"]
-    rownames(om) <- paste(om$week, om$state, sep='.')
-    test <- !is.na(om$internalFlow)
-    om[test, ]
-}
-om <- ReshapeAugmentObserved(observed=observed, fiAll=farms.and.inventory,
-                             flows=internal.flows)
-om <- merge(om, state.summaries, by='state')
-rownames(om) <- paste(om$week, om$state, sep='.')
-
-#' We've removed data where the flows are missing to allow to make sure
-#' that null models omitting that variable use the same data.
+om <- GetRegressionData(case.data=real.case.data, flows=internal.flows,
+                        fiAll=farms.and.inventory, stateCty=state.summaries)
 
 #' ## Model fitting
 #'
 #' Let's start with a least square fit on the logged response. We'll
 #' center the predictors around means to reduce the correlation
 #' between slopes and intercepts.
-
-iqr <- function(x) diff(quantile(x, probs=c(.25, .75), na.rm=TRUE))
-scaleiqr <- function(x) scale(x, scale=iqr(x))
-clog <- function(x) log(x + 0.5)
-
-om$logInternalFlowScaled <- scaleiqr(log(2 * om$internalFlow))
-om$weekCent <- scale(om$week, center=TRUE, scale=FALSE)
-om$logCmedDenseScaled <- scaleiqr(log(om$cmedDense * om$nFarms * om$nFarms))
-om$logMDenseScaled <- scaleiqr(log(om$mDense * om$nFarms * om$nFarms))
-
-om$clogCases1wa <- clog(om$cases1WksAgo)
-om$clogCases2wa <- clog(om$cases2WksAgo)
-om$clogCases3wa <- clog(om$cases3WksAgo)
-om$clogCases4wa <- clog(om$cases4WksAgo)
-om$clogCases1wa05 <- log(om$cases1WksAgo + 0.05)
-om$clogCases1wa005 <- log(om$cases1WksAgo + 0.005)
 
 m <- list()
 f <- list()
