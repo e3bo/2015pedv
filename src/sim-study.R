a83428a8f799afadc4c05ccf218324bf08f4c4ef
Eamon O'Dea <odea35@gmail.com>
2015-10-27 15:41:06 -0400

    Start adding Gaussian process metamodel

--- src/sim-study.R
+++ src/sim-study.R
@@ -3,7 +3,7 @@
 agent.args <- list(target.mean.deg=1, census.dilation=.1)
 ag <- do.call(aporkalypse::CreateAgents, agent.args)
 
-sim.args <- data.frame(rep=1:2, starting.state='IA', tprob.sp=1, tprob.net=1,
+sim.args <- data.frame(starting.state='NC', tprob.sp=1, tprob.net=seq(0, 0.4, len=10),
                        rprob=0.5, stringsAsFactors=FALSE)
 df <- cbind(agent.args, sim.args)
 
@@ -14,5 +14,16 @@ res <- Map(aporkalypse::SimulateAndSummarize, starting.state=df$starting.state,
 tstats <- lapply(res, '[[', 'mantel.tests')
 dfsplt <- split(df, seq(nrow(df)))
 resplt <- Map(cbind, dfsplt, tstats)
-do.call(rbind, resplt)
+resall <- do.call(rbind, resplt)
 
+test <- resall$symmetrize == TRUE & resall$mat2.name == 'shipment' & resall$method== 'spearman'
+sub <- resall[test, ]
+
+
+fit <- kernlab::gausspr(r~tprob.net, data=sub, var=2, kernel='laplacedot')
+plot(r~tprob.net, data=sub)
+x <- sub[, 'tprob.net', drop=FALSE]
+pred <- predict(fit, x)
+lines(cbind(x, pred))
+
+plot(pValues~tprob.net, data=sub)
