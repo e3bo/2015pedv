a4f7915ad0b24ca94d3ebd23f21dd1c529fcda75
Eamon O'Dea <odea35@gmail.com>
2015-12-03 16:45:42 -0500

    Switch build system to use gcd response
    Layers 4 and 5 now look for an environmental variable to determine
    which response variable to choose. This makes it easy to launch a
    different r job for each process. It By branching at the level of the
    Makefile, we prevent the need for any single R process to keep the
    results of all the different responses in memory. This way, the build
    can occur on machines with only 1 GB of memory, and memory
    requirements with the number of jobs active at any time rather than
    the total number of jobs.

--- src/sim-study-layer04.R
+++ src/sim-study-layer04.R
@@ -2,20 +2,24 @@
 library(methods) #for raster
 
 set.seed(124, "L'Ecuyer")
-options('mc.cores'=sds::GetCores())
+options('mc.cores'=1)
 
 load('sim-study-checkpoint3.rda')
 
-kms <- sds::GetMetaModels(resall, df, var2="gcd")
-save.image('sim-study-checkpoint4.rda')
-saveRDS(kms$m2$model, "kmm2.rds")
-saveRDS(kms$v2$model, "kmv2.rds")
-saveRDS(kms$center, "center.rds")
+var2 <- Sys.getenv('var2')
+if (nchar(var2) == 0) {
+  var2 <- 'gcd'
+}
+kms <- sds::GetMetaModels(resall, df, var2=var2)
+save.image(paste0('sim-study-checkpoint4-', var2, '.rda'))
+saveRDS(kms$m2$model, paste0("kmm2-", var2, ".rds"))
+saveRDS(kms$v2$model, paste0("kmv2-", var2, ".rds"))
 
 tp.ind <- which(names(kms$center) == 'tprob.net')
 tp.sp.ind <- which(names(kms$center) == 'tprob.sp')
 sa.ind <- which(names(kms$center) == 'seasonal.amplitude')
-pdf('km-m2-views.pdf')
+file <- paste0('km-m2-views-', var2, '.pdf')
+pdf(file)
 DiceView::sectionview(kms$m2$model, axis=tp.ind, center=kms$center, mfrow=c(1,1))
 DiceView::sectionview(kms$m2$model, axis=tp.sp.ind, center=kms$center, mfrow=c(1,1))
 DiceView::sectionview(kms$m2, axis=tp.ind, center=kms$center, mfrow=c(1,1))
--- src/sim-study-layer05.R
+++ src/sim-study-layer05.R
@@ -2,16 +2,19 @@
 library(methods) #for raster
 
 set.seed(125, "L'Ecuyer")
-options('mc.cores'=sds::GetCores())
+options('mc.cores'=1)
 
-kmm2 <- readRDS("kmm2.rds")
-kmv2 <- readRDS("kmv2.rds")
-center <- readRDS("center.rds")
+var2 <- Sys.getenv('var2')
+if (nchar(var2) == 0) {
+  var2 <- 'gcd'
+}
+kmm2 <- readRDS(paste0("kmm2-", var2, ".rds"))
+kmv2 <- readRDS(paste0("kmv2-", var2, ".rds"))
 all.par.ranges <- readRDS("all.par.ranges.rds")
-nmeta <- 1e4
+nmeta <- 1e3
 
 sob.out <- sds::RunSobol(nmeta, kmm2=kmm2, kmv2=kmv2, all.par.ranges=all.par.ranges,
                          order=2)
-save.image('sim-study-checkpoint5.rda')
+save.image(paste0('sim-study-checkpoint5-', var2, '.rda'))
 
 sob.out
