f2aa13a8b11acd0a727968de8b6c71af3ba511e0
Eamon O'Dea <odea35@gmail.com>
2015-12-06 23:44:46 -0500

    Change design of SA
    Fix target mean degree so as to shrink dimensionality a bit. It seems
    as if fitting a Gaussian process to much more than 1000 points could
    be burdensome, like an hour or so with bfgs. So it's not clear that we
    can afford another dimension and the effect of the mean degree seems
    fairly clear without including it. We still keep the raster cell area
    varied because it would be interesting to show a point where it is
    responsive to tprob.sp. At the 100 mi^2 setting, it looked like the
    correlation saturated very quickly like before tprob.sp == 0.05. At
    the 4 mi^2 setting, which might be more realistic for PEDV, there is
    little sensitivity to tprob.sp.

--- src/sim-study-layer01.R
+++ src/sim-study-layer01.R
@@ -5,9 +5,9 @@ set.seed(121, "L'Ecuyer")
 
 options('mc.cores'=sds::GetCores())
 
-target.mean.deg.grid <- c(5, 10) #seq(0.1, 10.1, by=10)
-raster.ncol.grid <- c(2850 / 2, 285) # 285 x 178 corresponds to roughly (16 km)^2 square cells
-raster.nrow.grid <- c(1780 / 2, 178)
+target.mean.deg.grid <- 1:10
+raster.ncol.grid <- c(1425, 570) # 285 x 178 corresponds to roughly (16 km)^2 square cells
+raster.nrow.grid <- c(890, 356)
 
 ag <- parallel::mcMap(sds::CreateAgents,
                       target.mean.deg=target.mean.deg.grid[1],
